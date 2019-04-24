af7180573eb7b145578f1caacfd3fd59fe286b09
Guandjoy <guandjoy@gmail.com>
2019-04-24 17:27:46 +0300

    Style with linaria

--- gatsby-app/src/components/header.js
+++ gatsby-app/src/components/header.js
@@ -1,27 +1,27 @@
 import { Link } from "gatsby"
+import { css } from "linaria"
 import PropTypes from "prop-types"
 import React from "react"
-import logo from '../images/logo.svg'
+
+const logo = css`
+  width: 64px;
+  height: 64px;
+  border-radius: 1px;
+  background: url(../images/logo.svg);
+  background-size: 42px 42px;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-color: #FDFDFD;
+  position: absolute;
+  top: 24px;
+  left: 32px;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+`
 
 const Header = ({ siteTitle }) => (
   <header>
-    <a 
-      href="/"
-      style={{
-        width: "64px",
-        height: "64px",
-        borderRadius: "1px",
-        background: `url(${logo})`,
-        backgroundSize: "42px 42px",
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        backgroundColor: '#fafafa',
-        position: 'absolute',
-        top: '24px',
-        left: '32px',
-        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
-      }}
-    />
+    <a href="/" className={logo} />
   </header>
 )
 
@@ -34,3 +34,4 @@ Header.defaultProps = {
 }
 
 export default Header
+
--- gatsby-app/src/pages/index.js
+++ gatsby-app/src/pages/index.js
@@ -5,26 +5,58 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import { css, cx } from "linaria"
+
+const header = css`
+  color: #414141;
+  position: absolute;
+  left: 260px;
+  top: 260px;
+`
+
+const hero = css`
+  width: 100%;
+  height: 80vh;
+  background-color: #F0F0F0;
+`
+
+const section = css`
+  width: 100%;
+  height: 80vh;
+
+  .title {
+    color: white;
+  }
+`
+
+const getInTouch = css`
+  background-color: #100925;
+`
+
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <div style={{
-      width: '100%',
-      height: '80vh',
-      backgroundColor: 'grey',
-    }}>
-      <h1 style={{
-        position: 'relative',
-        left: '260px',
-        top: '260px',
-      }}>Hi people</h1>
+    <div className={hero}>
+      <h1 className={header}>Hi people</h1>
     </div>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className={section}>
+      <div>
+        <a href="https://dribbble.com/Guandjoy" target="_blank">Dribbble</a>
+        <p>Here I post shots of what I'm doing</p>
+      </div>
+      <div>
+        <a href="https://twitter.com/guandjoy" target="_blank">Twitter</a>
+        <p>The place where I try to be on edge</p>
+      </div>
+      <div>
+        <a href="https://github.com/guandjoy" target="_blank">GitHub</a>
+        <p>The place with sources. If you want to dive deeper and look under the hood</p>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className={cx(section, getInTouch)}>
+      <h3 className="title">Get in touch</h3>
+    </div>
   </Layout>
 )
 
