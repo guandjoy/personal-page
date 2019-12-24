aa18410b6ff5226d01a61ef08f45ba6d8d5bf448
guandjoy <guandjoy@gmail.com>
2019-12-24 23:36:09 +0200

    Update all social icons

---
+++ gatsby-app/src/images/FbIcon.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FbIcon(props) {
+  return (
+    <svg viewBox="0 0 438 824" {...props}>
+      <path d="M409.3 460L432 312H290v-96c0-40.5 19.8-80 83.4-80H438V10S379.4 0 323.4 0C206.4 0 130 70.9 130 199.2V312H0v148h130v357.8c26.1 4.1 52.8 6.2 80 6.2s53.9-2.1 80-6.2V460h119.3z" />
+    </svg>
+  );
+}
+
+export default FbIcon;
---
+++ gatsby-app/src/images/LinkedInIcon.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LinkedInIcon(props) {
+  return (
+    <svg viewBox="0 0 19 18" className="prefix__d-block" {...props}>
+      <path d="M3.94 2A2 2 0 112 0a2 2 0 011.94 2zM4 5.48H0V18h4zm6.32 0H6.34V18h3.94v-6.57c0-3.66 4.77-4 4.77 0V18H19v-7.93c0-6.17-7.06-5.94-8.72-2.91z" />
+    </svg>
+  );
+}
+
+export default LinkedInIcon;
