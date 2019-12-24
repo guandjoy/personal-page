9846c3387277235697dfc764ecad8dced82e9e79
guandjoy <guandjoy@gmail.com>
2019-12-25 01:59:38 +0200

    Add links on social buttons

--- gatsby-app/src/components/hero/SocialButtons/SocialButtons.js
+++ gatsby-app/src/components/hero/SocialButtons/SocialButtons.js
@@ -10,11 +10,22 @@ import "./SocialButtons.css";
 function SocialButtons() {
   return (
     <div className="social-buttons__wrapper">
-      <TwitterIcon className="social-buttons__item" />
-      <GitHubIcon className="social-buttons__item" />
-      <InstagramIcon className="social-buttons__item" />
-      {/* <LinkedInIcon className="social-buttons__item" /> */}
-      <FbIcon id="fb-icon" className="social-buttons__item" />
+      <a href="https://twitter.com/guandjoy">
+        <TwitterIcon className="social-buttons__item" />
+      </a>
+      <a href="https://github.com/guandjoy">
+        <GitHubIcon className="social-buttons__item" />
+      </a>
+
+      <a href="https://www.instagram.com/yurko.turskiy/">
+        <InstagramIcon className="social-buttons__item" />
+      </a>
+      {/* <a href="https://www.linkedin.com/in/yurko-turkiy">
+        <LinkedInIcon className="social-buttons__item" />
+      </a> */}
+      <a href="https://www.facebook.com/guandjoy">
+        <FbIcon id="fb-icon" className="social-buttons__item" />
+      </a>
     </div>
   );
 }
