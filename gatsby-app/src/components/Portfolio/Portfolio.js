4ab0f14390c5fa26bf30a67ffc67b3264403326c
guandjoy <guandjoy@gmail.com>
2019-12-30 03:16:22 +0200

    Init Primitivo

--- gatsby-app/src/components/Portfolio/Portfolio.js
+++ gatsby-app/src/components/Portfolio/Portfolio.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Rudl from "./Rudl";
+import Primitivo from "./Primitivo";
 
 function Portfolio(props) {
   return (
     <div className="o-portfolio__wrapper">
       <Rudl />
-      <h2>Primitivo-SVG</h2>
+      <Primitivo />
       <h2>Redfish</h2>
     </div>
   );
---
+++ gatsby-app/src/components/Portfolio/Primitivo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Primitivo({}) {
+  return <h2>Primitivo-SVG</h2>;
+}
+
+export default Primitivo;
