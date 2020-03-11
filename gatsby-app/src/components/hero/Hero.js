babc1bbb258c25cc78d36134afe746f18011db6b
Yurko Turskiy <guandjoy@gmail.com>
2020-03-11 19:56:36 +0200

    Add hero onLoad event

--- gatsby-app/src/components/hero/Hero.js
+++ gatsby-app/src/components/hero/Hero.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import SocialButtons from "./SocialButtons/SocialButtons";
 import Video from "./Video";
 import Logo from "./Logo";
@@ -10,10 +10,13 @@ import "./Hero.css";
 function Hero() {
   const [videoIsReady, setVideoIsReady] = useState(false);
   const [soleIsLoaded, setSoleIsLoaded] = useState(false);
-  useEffect(() => console.log("video", videoIsReady), [videoIsReady]);
-  useEffect(() => console.log("sole", soleIsLoaded), [soleIsLoaded]);
+  const [heroIsLoaded, setHeroIsLoaded] = useState(false);
   return (
-    <div style={{ opacity: soleIsLoaded ? "1" : "0" }} className="o-hero">
+    <div
+      style={{ opacity: heroIsLoaded ? "1" : "0" }}
+      className="o-hero"
+      onLoad={() => setHeroIsLoaded(true)}
+    >
       <div className="m-elements">
         <Logo />
         <h1 className="m-elements__header">Is There Web On Mars?</h1>
