4e4dec3e9a8563712fc811b049476e1ef78b2463
Yurko Turskiy <guandjoy@gmail.com>
2020-03-11 18:10:51 +0200

    Show hero on load

--- gatsby-app/src/components/hero/Hero.js
+++ gatsby-app/src/components/hero/Hero.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import SocialButtons from "./SocialButtons/SocialButtons";
 import Video from "./Video";
 import Logo from "./Logo";
@@ -7,8 +7,10 @@ import ListeningStatus from "./ListeningStatus/ListeningStatus";
 import "./Hero.css";
 
 function Hero() {
+  const [videoIsReady, setVideoIsReady] = useState(false);
+  useEffect(() => console.log(videoIsReady), [videoIsReady]);
   return (
-    <div className="o-hero">
+    <div style={{ opacity: videoIsReady ? "1" : "0" }} className="o-hero">
       <div className="m-elements">
         <Logo />
         <h1 className="m-elements__header">Is There Web On Mars?</h1>
@@ -18,7 +20,7 @@ function Hero() {
         <SocialButtons />
       </div>
       <div className="m-video__wrapper">
-        <Video />
+        <Video setVideoIsReady={setVideoIsReady} />
         <ListeningStatus />
       </div>
     </div>
--- gatsby-app/src/components/hero/Video.js
+++ gatsby-app/src/components/hero/Video.js
@@ -1,12 +1,18 @@
 import React, { useEffect } from "react";
 
 function Video(props) {
-  // useEffect(() => {
-  //   document.getElementById("hero-video").play();
-  // }, []);
-  // http://yurko-personal-page.s3.eu-central-1.amazonaws.com/HeroVideo.mp4
+  useEffect(() => {
+    document.getElementById("hero-video").play();
+  }, []);
+
   return (
-    <video autoPlay muted loop id="hero-video">
+    <video
+      onCanPlay={() => props.setVideoIsReady(true)}
+      autoPlay
+      muted
+      loop
+      id="hero-video"
+    >
       <source
         src="https://yurko-personal-page.s3.eu-central-1.amazonaws.com/HeroVideo-FullHD-4-bitrate.mp4"
         type="video/mp4"
