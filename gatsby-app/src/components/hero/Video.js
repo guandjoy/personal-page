6e76605176cf0a6c5e81e6f4d2a0126f3e1cfbd5
Yurko Turskiy <guandjoy@gmail.com>
2020-03-12 00:22:03 +0200

    Lossy video quality for mobileOnly devices

--- gatsby-app/src/components/hero/Video.js
+++ gatsby-app/src/components/hero/Video.js
@@ -1,22 +1,19 @@
 import React, { useEffect } from "react";
+import { isMobileOnly } from "react-device-detect";
 
-function Video(props) {
+function Video() {
   useEffect(() => {
     document.getElementById("hero-video").play();
   }, []);
 
+  // Lossy video for mobile only devices
+  const sourcePath = isMobileOnly
+    ? "https://yurko-personal-page.s3.eu-central-1.amazonaws.com/HeroVideo-FullHD-1-bitrate.mp4"
+    : "https://yurko-personal-page.s3.eu-central-1.amazonaws.com/HeroVideo-FullHD-4-bitrate.mp4";
+
   return (
-    <video
-      onCanPlay={() => props.setVideoIsReady(true)}
-      autoPlay
-      muted
-      loop
-      id="hero-video"
-    >
-      <source
-        src="https://yurko-personal-page.s3.eu-central-1.amazonaws.com/HeroVideo-FullHD-4-bitrate.mp4"
-        type="video/mp4"
-      />
+    <video autoPlay muted loop id="hero-video">
+      <source src={sourcePath} type="video/mp4" />
       Sorry, your browser doesn't support embedded videos.
     </video>
   );
