5dc20cdae07815a1f4abfacd765e9a63721e0269
Yurko Turskiy <guandjoy@gmail.com>
2020-03-12 00:22:50 +0200

    Add sole validation for SSR

--- gatsby-app/src/components/hero/FirstFrameSole.js
+++ gatsby-app/src/components/hero/FirstFrameSole.js
@@ -1,12 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback, useRef } from "react";
 
-function FirstFrameSole({ setSoleIsLoaded }) {
-  useEffect(() => setSoleIsLoaded(true));
+function FirstFrameSole({ setSoleIsLoaded, soleIsLoaded }) {
+  const soleRef = useRef(null);
+
+  const onLoad = useCallback(() => setSoleIsLoaded(true), []);
+
+  useEffect(() => {
+    // Check if sole is loaded. Method for SSR
+    soleRef.current.complete
+      ? onLoad()
+      : soleRef.current.addEventListener("load", onLoad);
+    return () => {
+      soleRef.current.removeEventListener("load", onLoad);
+    };
+  }, []);
+
   return (
     <img
+      ref={soleRef}
       src="https://yurko-personal-page.s3.eu-central-1.amazonaws.com/FirstFrame.jpg"
       className="m-video__first-frame-sole"
-      // onLoad={() => setSoleIsLoaded(true)}
     />
   );
 }
--- gatsby-app/src/components/hero/Hero.js
+++ gatsby-app/src/components/hero/Hero.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import SocialButtons from "./SocialButtons/SocialButtons";
 import Video from "./Video";
 import Logo from "./Logo";
@@ -8,15 +8,10 @@ import FirstFrameSole from "./FirstFrameSole";
 import "./Hero.css";
 
 function Hero() {
-  const [videoIsReady, setVideoIsReady] = useState(false);
+  // Show hero container only if sole is loaded
   const [soleIsLoaded, setSoleIsLoaded] = useState(false);
-  const [heroIsLoaded, setHeroIsLoaded] = useState(false);
   return (
-    <div
-      style={{ opacity: heroIsLoaded ? "1" : "0" }}
-      className="o-hero"
-      onLoad={() => setHeroIsLoaded(true)}
-    >
+    <div style={{ opacity: soleIsLoaded ? "1" : "0" }} className="o-hero">
       <div className="m-elements">
         <Logo />
         <h1 className="m-elements__header">Is There Web On Mars?</h1>
@@ -26,8 +21,12 @@ function Hero() {
         <SocialButtons />
       </div>
       <div className="m-video__wrapper">
-        <Video setVideoIsReady={setVideoIsReady} />
-        <FirstFrameSole setSoleIsLoaded={setSoleIsLoaded} />
+        <Video />
+        <FirstFrameSole
+          soleIsLoaded={soleIsLoaded}
+          setSoleIsLoaded={setSoleIsLoaded}
+        />
+        {/* <div className="m-video__bg" /> */}
         <ListeningStatus />
       </div>
     </div>
