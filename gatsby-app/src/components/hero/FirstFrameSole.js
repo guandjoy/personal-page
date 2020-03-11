62877656b1ed047bf131bd6b4cad8be788db8666
Yurko Turskiy <guandjoy@gmail.com>
2020-03-11 19:12:48 +0200

    Set soleIsLoaded on component is mount

--- gatsby-app/src/components/hero/FirstFrameSole.js
+++ gatsby-app/src/components/hero/FirstFrameSole.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function FirstFrameSole({ setSoleIsLoaded }) {
+  useEffect(() => setSoleIsLoaded(true));
   return (
     <img
       src="https://yurko-personal-page.s3.eu-central-1.amazonaws.com/FirstFrame.jpg"
       className="m-video__first-frame-sole"
-      onLoad={() => setSoleIsLoaded(true)}
+      // onLoad={() => setSoleIsLoaded(true)}
     />
   );
 }
