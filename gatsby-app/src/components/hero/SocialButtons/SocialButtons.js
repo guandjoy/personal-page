9c0bb7fdcb800becaa1fc54bae5c55fa0742ad11
Yurko Turskiy <guandjoy@gmail.com>
2020-03-13 22:22:29 +0200

    Include instagram icon

--- gatsby-app/src/components/hero/SocialButtons/SocialButtons.js
+++ gatsby-app/src/components/hero/SocialButtons/SocialButtons.js
@@ -3,8 +3,8 @@ import Icon from "./Icon";
 // Social SVG Icons
 import TwitterIcon from "../../../images/icons/TwitterIcon";
 import GitHubIcon from "../../../images/icons/GitHubIcon";
-// import InstagramIcon from "../../../images/icons/InstagramIcon";
 // import LinkedInIcon from "../../../images/icons/LinkedInIcon";
+import InstagramIcon from "../../../images/icons/InstagramIcon";
 // import FbIcon from "../../../images/icons/FbIcon";
 // import RedditIcon from "../../../images/icons/RedditIcon";
 import DribbbleIcon from "../../../images/icons/DribbbleIcon";
@@ -15,10 +15,10 @@ function SocialButtons() {
       <Icon Component={TwitterIcon} url="https://twitter.com/guandjoy/" />
       <Icon Component={DribbbleIcon} url="https://dribbble.com/Guandjoy" />
       <Icon Component={GitHubIcon} url="https://github.com/guandjoy" />
-      {/* <Icon */}
-      {/*   Component={InstagramIcon} */}
-      {/*   url="https://instagram.com/yurko.turskiy/" */}
-      {/* /> */}
+      <Icon
+        Component={InstagramIcon}
+        url="https://instagram.com/yurko.turskiy/"
+      />
       {/* <Icon */}
       {/*   Component={FbIcon} */}
       {/*   id="fb-icon" */}
