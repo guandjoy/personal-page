ea188829978ddcc79bfd6cb50c24ef42f7b18f55
Yurko Turskiy <guandjoy@gmail.com>
2020-06-23 21:30:50 +0300

    Replace Lemon video to Tambourine

--- gatsby-app/src/components/hero/ListeningStatus/ListeningStatus.js
+++ gatsby-app/src/components/hero/ListeningStatus/ListeningStatus.js
@@ -5,13 +5,13 @@ function ListeningStatus(props) {
   return (
     <div className="m-listening-status">
       <a
-        href="https://youtu.be/L_u97PqWX6g?t=44"
+        href="https://youtu.be/Zzej0ETmg4Y"
         target="_blank"
         className="m-listening-status__link"
         rel="noopener noreferrer"
       >
         <SoundIcon className="m-listening-status__icon" />
-        <span>N.E.R.D & Rihanna – Lemon</span>
+        <span>Eve – Tambourine (Explicit)</span>
       </a>
     </div>
   );
